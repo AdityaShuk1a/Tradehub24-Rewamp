4b209163b6d76bf51f3b4f7f9e7717663320a495
FarSpawn <abhijitabhi127@gmail.com>
2025-01-26 18:11:46 +0530

    setting up colors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,6 +20,18 @@ import HireSafely from '../components/HireSafely.jsx';
 //bdf
 //e99f9f
 
+const topL = '#1D8189'
+const botR = '#B89D91'
+const innerLight = '#767572'
+const butRed ="#CAAA9E"
+const fillLightP2 = '#EEE3DB'
+const p3topR = '#2C8796'
+const p3botL = "#4E6E72"
+const p3CardDark = '#EEE3DB'
+const p3CardLight = '#F7F4F3'
+const CardDark = '#151114'
+
+
 const Home = () => {
 
   return (
@@ -27,12 +39,12 @@ const Home = () => {
       <div className='container-1 h-full bg-[#b3d6fa] flex flex-col z-10 rounded-b-[7vh] rounded-bl-[7vh]'>
         <div className='container-2 h-full bg-[#53678d] flex flex-col rounded-b-[7vh] rounded-bl-[7vh]'>
           <div className='container-3 h-full bg-[#b3d6fa] flex flex-col rounded-b-[7vh] rounded-bl-[7vh]'>
-            <div className="container-4 h-[210vh] w-screen bg-[#000] flex flex-col  rounded-b-[7vh] rounded-bl-[7vh]">
-              <div className="container-5 h-[110vh] w-screen bg-[#b3d6fa] justify-center rounded-b-[7vh] rounded-bl-[7vh]">
+            <div className="container-4 h-[210vh] w-screen bg-[#0f0] flex flex-col  rounded-b-[7vh] rounded-bl-[7vh]">
+              <div className="container-5 h-[110vh] w-screen bg-gradient-to-br from-[#26c6f7] to-[#c1c58d] justify-center rounded-b-[7vh] rounded-bl-[7vh]">
                 <Navbar />
                 <div className='flex w-screen' style={{ marginTop: "7vh" }}>
-                  <div className="container-7 h-[50vh] bg-[#268] w-1/2"> 
-                    <div className='flex flex-wrap w-[57vh] bg-amber-100 text-[7vh] gap-[1vh]' style={{ padding: "1.44vh" , marginLeft : "3vh"}}>
+                  <div className="container-7 h-[50vh]  w-1/2"> 
+                    <div className='flex flex-wrap w-[57vh] bg-transparent text-[7vh] gap-[1vh]' style={{ padding: "1.44vh" , marginLeft : "3vh"}}>
                       <span>Find</span>
                       <span>a</span>
                       <span>local</span>
@@ -45,12 +57,12 @@ const Home = () => {
                       <button className='bg-[#ff6e6e] rounded-full' style={{ marginTop: "1.92vh", padding: "0.96vh", fontSize: "1.92vh", width: "19.23vh" }}>start for free</button>
                     </div>
                   </div>
-                  <div className="container-8 h-[50vh] bg-[#268] w-1/2 flex justify-center items-center">
+                  <div className="container-8 h-[50vh]  w-1/2 flex justify-center items-center">
                     <img src={broImg} alt="" style={{ width: "38.46vh" }} />
                   </div>
                 </div>
                 <div className=' h-[23vh]  bg-[] ' style={{ margin : "8vh" }}>
-                  <div className='flex flex-wrap w-full h-[26.92vh] bg-[#bdf] rounded-[7vh] items-center justify-evenly'>
+                  <div className='flex flex-wrap w-full h-[26.92vh] border rounded-[7vh] items-center justify-evenly'>
                     <HowItWorks/>
                   </div>
                 </div>
